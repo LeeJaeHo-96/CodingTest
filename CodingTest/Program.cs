86638e1003c3c1edc3e5836a7f6d80acd3ad87a1
LeeJaeHo-96 <towpam123@gmail.com>
2024-08-12 12:09:38 +0900

    cote

---
+++ CodingTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodingTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double solution(int numbers[], size_t numbers_len)
+            {
+
+                for (int i = 0; i < numbers_len; i++)
+                {
+                    int x = numbers[i] +;
+
+                }
+                
+
+
+    double answer = 0;
+                return answer;
+            }
+    }
+}
---
+++ Codingtest 문자열뒤집기/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Codingtest_문자열뒤집기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                
+            }
+        }
+        public class Solution
+        {
+            public string solution(string my_string)
+            {
+                string answer = "";
+                char[] arr = my_string.ToCharArray();
+                Array.Reverse(arr);
+                answer = new string(arr);
+                return answer;
+            }
+        }
+    }
+}
